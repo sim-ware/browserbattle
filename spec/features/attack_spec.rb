5ce91c18072286ca165dea76eeb686bf73a62799
Samir Gossain <samir.gossain@gmail.com>
2017-08-22 16:36:33 +0100

    Adds HP functionality

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,16 @@
 feature 'Attacking' do
-  scenario 'attack Player 2' do
+  feature 'Attacking' do
+    scenario 'attack Player 2' do
+      sign_in_and_play
+      click_link 'Attack'
+      expect(page).to have_content 'Simi attacked Luna'
+    end
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_link 'Attack'
-    expect(page).to have_content 'Simi attacked Luna'
+    expect(page).not_to have_content 'Luna: 60HP'
+    expect(page).to have_content 'Luna: 50HP'
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,30 @@ require 'player'
 
 describe Player do
   subject(:simi) { Player.new('Simi') }
+  subject(:luna) { Player.new('Luna') }
 
   describe '#name' do
     it 'returns the name' do
       expect(simi.name).to eq 'Simi'
     end
   end
-end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(simi.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(luna).to receive(:receive_damage)
+      simi.attack(luna)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { simi.receive_damage }.to change { simi.hit_points }.by(-10)
+    end
+  end
+ end
